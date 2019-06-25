d6c30e00a58c2bc549f82647a5b67d3bd2249180
Humberto <codycinder@hotmail.com>
2019-06-24 23:05:24 -0500

    Referencing controller bean from application context

--- src/main/java/guru/springframework/didemo/controllers/MyController.java
+++ src/main/java/guru/springframework/didemo/controllers/MyController.java
@@ -5,4 +5,9 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
+
+    public String hello(){
+        System.out.println("Hello World!!!");
+        return "foo";
+    }
 }
